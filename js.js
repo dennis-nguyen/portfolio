07b33b5a9a4e7f99195516874860754ef0266085
dennis-nguyen <nguyen.denn@gmail.com>
2017-08-11 00:30:25 -0700

    updated skill and tools section

--- js.js
+++ js.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     // });
 
     $('.intro').typeIt({
-        strings: ["I am a programmer.", "I am a KBBQ enthusiast."],
+        strings: ["I am a web developer.", "I am a KBBQ enthusiast."],
         speed: 65,
         breakLines: false,
         autoStart: false,
@@ -24,6 +24,9 @@ $(document).ready(function () {
             zoom: 12,
             center: fv,
             disableDefaultUI: true
+            // scrollwheel: false,
+            // draggable: false
+
         });
         var marker = new google.maps.Marker({
             position: fv,
@@ -48,43 +51,46 @@ $(document).ready(function () {
 
 
     window.sr = ScrollReveal();
-    sr.reveal('.colored', {
-        duration: 1500
-    });
-    sr.reveal('#firebase', {
+    sr.reveal('.scrollReveal', {
         duration: 1500
     });
 
+    // $('body').scrollspy({target: ".navbar"})
 
-// $('body').scrollspy({target: ".navbar"})
-
-// Add scrollspy to <body>
-$('body').scrollspy({target: ".navbar", offset: 50});
-
-// Add smooth scrolling on all links inside the navbar
-$("#navbar a").on('click', function(event) {
-
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
-
-    // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
+    // Add scrollspy to <body>
+    $('body').scrollspy({
+        target: ".navbar",
+        offset: 50
     });
 
-  } // End if
+    // Add smooth scrolling on all links inside the navbar
+    $("#navbar a").on('click', function (event) {
 
-});
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+
+        } // End if
+
+    });
+
+
+
+
 
 });
